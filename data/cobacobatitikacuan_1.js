465caeec15c879b678e7a805b657fde49cdc16c1
nurmalika <76059355+nurmalika@users.noreply.github.com>
2020-12-15 23:16:56 +0700

    Add files via upload

---
+++ data/cobacobatitikacuan_1.js
@@ -0,0 +1,8 @@
+var json_cobacobatitikacuan_1 = {
+"type": "FeatureCollection",
+"name": "cobacobatitikacuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.601217213809647, -7.532387788021572 ] } }
+]
+}
